ed4ebe414658014b4236d4fc86c4b6cb0b1c4a6b
mirrdy <66448999+mirrdy@users.noreply.github.com>
2023-03-28 18:13:52 +0900

    20230328 DY3

---
+++ Console_Pokemon_Project/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Pokemon_Project
+{
+    class GameManager
+    {
+        public static void Start()
+        {
+            Screen.PreferencesWindow();
+            Map map = new Map(0, 0);
+            while (true)
+            {
+                map.WaitPlayerInput();
+            }
+
+            // Battle battle = new Battle();
+
+            //Screen.print(pixel);
+            //if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+            //{
+            //  Menu.SelectMenu(cursorX, cursorY, option);
+            //}
+        }
+    }
+}
--- Console_Pokemon_Project/Program.cs
+++ Console_Pokemon_Project/Program.cs
@@ -10,25 +10,7 @@ namespace Console_Pokemon_Project
     {
         static void Main(string[] args)
         {
-            Start();
-        }
-
-        public static void Start()
-        {
-            Screen.PreferencesWindow();
-            Map map = new Map(0, 0);
-            while (true)
-            {
-                map.WaitPlayerInput();
-            }
-
-            // Battle battle = new Battle();
-
-            //Screen.print(pixel);
-            //if (Console.ReadKey(true).Key == ConsoleKey.Escape)
-            //{
-            //  Menu.SelectMenu(cursorX, cursorY, option);
-            //}
+            GameManager.Start();
         }
     }
 }
