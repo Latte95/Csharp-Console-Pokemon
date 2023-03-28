a4773450690815e0134c41cc5f0927017d004178
Latte95 <123979439+Latte95@users.noreply.github.com>
2023-03-29 00:41:38 +0900

    20230328 Add EquipmentSlot

--- Console_Pokemon_Project/EquipmentSlot.cs
+++ Console_Pokemon_Project/EquipmentSlot.cs
@@ -8,11 +8,106 @@ namespace Console_Pokemon_Project
 {
     public class EquipmentSlot
     {
-        List<Item> equipItem;
+        // 딕셔너리를 쓴 이유 : 키값을 하나밖에 못 갖으니까, 키값을 슬롯으로 활용
+        private Dictionary<EquipableItem.EQUIPTYPE, EquipableItem> equipSlots;
 
         public EquipmentSlot()
         {
-            equipItem = new List<Item>();
+            equipSlots = new Dictionary<EquipableItem.EQUIPTYPE, EquipableItem>();
+            // EQUIPTYPE에 있는 슬롯마다 null로 초기화
+            foreach (EquipableItem.EQUIPTYPE type in Enum.GetValues(typeof(EquipableItem.EQUIPTYPE)))
+            {
+                equipSlots.Add(type, null);
+            }
+        }
+
+        // 장비 착용
+        public void EquipItem(EquipableItem item)
+        {
+            Inventory inventory = Player.instance.inven;
+            // 착용할 장비의 부위에
+            EquipableItem.EQUIPTYPE type = item.equipType;
+            // 이미 다른 아이템이 착용되어 있으면
+            if (equipSlots[type] != null)
+            {
+                // 착용되어 있던 장비는 해제하고
+                UnequipItem(equipSlots[type]);
+            }
+            // 새로운 아이템 착용한 뒤
+            equipSlots[type] = item;
+            // 인벤토리에서 해당 아이템은 제거
+            inventory.RemoveItem(item);
+
+        }
+
+        public void UnequipItem(EquipableItem item)
+        {
+            Inventory inventory = Player.instance.inven;
+            // 장비하고 있는 슬롯을 비우고
+            equipSlots[item.equipType] = null;
+            // 인벤토리에 해당 장비를 추가
+            inventory.AddItem(item);
+        }
+
+
+            //public void Equip(EquipableItem item)
+            //{
+            //    int type = (int)item.equipType;
+
+            //    EquipableItem tmpItem = new EquipableItem(item.name, item.atk, item.def, item.equipType, item.price);
+            //    item = equipInfo[type];
+            //    equipInfo[type] = tmpItem;
+            //}
+            //public void EquipFromInven(int invenIndex)
+            //{
+            //    EquipableItem tmpItem = this.inven.itemList[invenIndex] as EquipableItem;
+
+            //    // 인벤 장착 선택한 아이템이 장비템이 아니면 종료
+            //    if (tmpItem is null)
+            //    {
+            //        return;
+            //    }
+            //    // 선택 인덱스가 아이템 리스트크기를 넘으면 종료
+            //    if (invenIndex >= this.inven.itemList.Count)
+            //    {
+            //        return;
+            //    }
+
+            //    // 현재 장착중인 장비가 없으면 인벤토리에 추가할 아이템 없음
+            //    EquipableItem myEquipItem = this.equipInfo[(int)tmpItem.equipType];
+
+            //    // 장비창에 있는 아이템은 인벤토리로 넣고
+            //    if (myEquipItem is null == false)
+            //    {
+            //        this.inven.itemList.Add(equipInfo[(int)tmpItem.equipType]);
+            //    }
+            //    // 장착
+            //    Equip(tmpItem);
+            //    // 장비창으로 들어간 아이템은 인벤토리에서 삭제
+            //    this.inven.itemList.RemoveAt(invenIndex);
+            //}
+            //public int SumEquipAtk()
+            //{
+            //    int sum = 0;
+            //    int typeCount = Enum.GetValues(typeof(EquipableItem.EQUIPTYPE)).Length; // EQUIPTYPE의 총 개수
+            //    for (int i = 0; i < typeCount; i++)
+            //    {
+            //        if (this.equipInfo[i] == null)
+            //            continue;
+            //        sum += this.equipInfo[i].atk;
+            //    }
+            //    return sum;
+            //}
+            //public int SumEquipDef()
+            //{
+            //    int sum = 0;
+            //    for (int i = 0; i < 4; i++)
+            //    {
+            //        if (this.equipInfo[i] == null)
+            //            continue;
+            //        sum += this.equipInfo[i].def;
+            //    }
+            //    return sum;
+            //}
         }
     }
-}
--- Console_Pokemon_Project/Inventory.cs
+++ Console_Pokemon_Project/Inventory.cs
@@ -23,23 +23,23 @@ namespace Console_Pokemon_Project
             if (item is ConsumableItem)
             {
                 ConsumableItem addItem = (ConsumableItem)item;
-                bool found = false;
-                // 인벤토리에 있는 아이템 중
+                bool isExist = false;
+                // 인벤토리에
                 foreach (Item existItem in items)
                 {
-                    // 추가하는 아이템이 이미있으면 
+                    // 추가하는 아이템이 이미 있으면 
                     if (existItem.name == addItem.name)
                     {
                         // 이미 존재하는 아이템의
                         ConsumableItem existing = (ConsumableItem)existItem;
-                        // 개수를 추가하는 아이템 개수만큼 증가시키고
+                        // 개수를 추가하고
                         existing.quantity += addItem.quantity;
-                        found = true;
+                        isExist = true;
                         break;
                     }
                 }
                 // 없으면 새롭게 아이템을 추가
-                if (!found)
+                if (!isExist)
                 {
                     items.Add(addItem);
                 }
@@ -62,27 +62,24 @@ namespace Console_Pokemon_Project
             // 소비 아이템을 삭제할 땐
             if (item is ConsumableItem)
             {
-                ConsumableItem addItem = (ConsumableItem)item;
-                bool found = false;
+                ConsumableItem delItem = (ConsumableItem)item;
                 foreach (Item existItem in items)
                 {
-                    // 인벤토리에 삭제할 아이템이 있고
-                    if (existItem is ConsumableItem && existItem.name == addItem.name)
+                    // 인벤토리에 삭제할 아이템을 찾고
+                    if (existItem.name == delItem.name)
                     {
                         ConsumableItem existing = (ConsumableItem)existItem;
                         // 삭제할 개수보다 많이 가지고 있으면
-                        if (existing.quantity > addItem.quantity)
+                        if (existing.quantity > delItem.quantity)
                         {
                             // 해당 개수만큼만 삭제하고
-                            existing.quantity -= addItem.quantity;
-                            found = true;
+                            existing.quantity -= delItem.quantity;
                         }
                         // 보유한 수만큼 삭제하면
-                        else if (existing.quantity.Equals(addItem.quantity))
+                        else if (existing.quantity.Equals(delItem.quantity))
                         {
                             // 없애고
                             items.Remove(existing);
-                            found = true;
                         }
                         // 더 많이 삭제하면?
                         else
@@ -91,11 +88,6 @@ namespace Console_Pokemon_Project
                         }
                         break;
                     }
-                }
-                // 삭제할 아이템이 없으면 오류 출력
-                if (!found)
-                {
-                    // 수정
                 }
             }
             // 장비 아이템 삭제시엔 그냥 삭제
@@ -107,12 +99,6 @@ namespace Console_Pokemon_Project
             {
                 // 여기 아티팩트 구현? 수정
             }
-        }
-
-        // 인벤토리 아이템 출력
-        public List<Item> GetItems()
-        {
-            return new List<Item>(items);
         }
     }
 }
